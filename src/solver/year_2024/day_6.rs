7f6986404a4c9d91f97eaafe4382621a4bf071fc
Niashi <niasparacino4@gmail.com>
2024-12-06 19:46:10 -0500

    improve brute force

--- src/solver/year_2024/day_6.rs
+++ src/solver/year_2024/day_6.rs
@@ -40,41 +40,38 @@ impl CombinedSolver for Day6 {
                 Some(true) => direction = (-direction.1, direction.0),
             }
         }
+        let p_1 = visited.len();
         
         let mut p_2 = 0;
-        for y in 0..grid.w {
-            for x in 0..grid.h {
-                let pos = grid.get_mut(x, y).unwrap();
-                
-                if *pos || (x as i64, y as i64) == initial_position {
-                    continue;
-                }
-                
-                *pos = true;
-                
-                let mut visited = HashSet::new();
-                
-                let mut position = initial_position;
-                let mut direction = (0, -1);
-
-                loop {
-                    if !visited.insert((position, direction)) {
-                        p_2 += 1;
-                        break;
-                    }
-
-                    let new_pos = (position.0 + direction.0, position.1 + direction.1);
-                    match grid.get_i(new_pos.0, new_pos.1) {
-                        None => break,
-                        Some(false) => position = new_pos,
-                        Some(true) => direction = (-direction.1, direction.0),
-                    }
-                }
-                
-                *grid.get_mut(x, y).unwrap() = false;
+        for (x, y) in visited {
+            if (x, y) == initial_position {
+                continue;
             }
+            
+            *grid.get_mut(x as usize, y as usize).unwrap() = true;
+
+            let mut visited = HashSet::new();
+
+            let mut position = initial_position;
+            let mut direction = (0, -1);
+
+            loop {
+                if !visited.insert((position, direction)) {
+                    p_2 += 1;
+                    break;
+                }
+
+                let new_pos = (position.0 + direction.0, position.1 + direction.1);
+                match grid.get_i(new_pos.0, new_pos.1) {
+                    None => break,
+                    Some(false) => position = new_pos,
+                    Some(true) => direction = (-direction.1, direction.0),
+                }
+            }
+            
+            *grid.get_mut(x as usize, y as usize).unwrap() = false;
         }
         
-        Ok((visited.len().to_string(), p_2.to_string()))
+        Ok((p_1.to_string(), p_2.to_string()))
     }
 }
