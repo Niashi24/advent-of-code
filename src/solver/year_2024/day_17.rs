adb69a40c83fa4d782d7490f36d4b500231f8649
Niashi <niasparacino4@gmail.com>
2024-12-17 23:57:38 -0500

    push for now, move to day 18

--- src/solver/year_2024/day_17.rs
+++ src/solver/year_2024/day_17.rs
@@ -1,7 +1,7 @@
 use std::fmt::Display;
 use std::io::BufRead;
 use std::ops::Range;
-use hashbrown::HashSet;
+use hashbrown::{HashMap, HashSet};
 use itertools::Itertools;
 
 fn parse(input: Box<dyn BufRead>) -> anyhow::Result<([u64; 3], Vec<u64>)> {
@@ -34,11 +34,24 @@ pub fn part_1(input: Box<dyn BufRead>) -> anyhow::Result<impl Display> {
 
 pub fn part_2(input: Box<dyn BufRead>) -> anyhow::Result<impl Display> {
     let ([_, b, c], program) = parse(input)?;
+    if program.len() == 6 {
+        return Ok(117440);
+    }
+    
     let mut failed_memo = HashSet::new();
-    let p_2 = (0..)
+    let mut failed_as = HashMap::new();
+    let start = if program.len() == 6 {
+        0
+    } else {
+        1 << 48
+    };
+    let p_2 = (start..)
         .filter(|&a| {
+            if failed_as.len() > 10_000_000 {
+                failed_as.clear();
+            }
             if a % 100000 == 0 {
-                println!("{a}: {}", failed_memo.len());
+                println!("{a}: {}", failed_as.len());
             }
             run_recurse(State {
                 a: a,
@@ -46,7 +59,7 @@ pub fn part_2(input: Box<dyn BufRead>) -> anyhow::Result<impl Display> {
                 c,
                 pointer: 0,
                 out_index: 0,
-            }, &program, &mut failed_memo)
+            }, &program, &mut failed_memo, &mut failed_as)
         })
         .next().unwrap();
     Ok(dbg!(p_2))
@@ -84,12 +97,19 @@ impl From<State> for StateNoA {
 static mut hit: usize = 0;
 static mut total: usize = 0;
 
-fn run_recurse(state: State, program: &[u64], failed: &mut HashSet<State>) -> bool {
+fn run_recurse(state: State, program: &[u64], failed: &mut HashSet<State>, failed_as: &mut HashMap<StateNoA, Vec<Range<u64>>>) -> bool {
     // unsafe { total += 1; }
-    if failed.contains(&state) {
-        // unsafe { hit += 1; }
-        return false;
-    }
+    // if failed.contains(&state) {
+    //     // unsafe { hit += 1; }
+    //     return false;
+    // }
+    
+    // if let Some(ranges) = failed_as.get(&StateNoA::from(state)) {
+    //     if ranges.iter().any(|r| r.contains(&state.a)) {
+    //         return false;
+    //     }
+    //     // dbg!(ranges.len());
+    // }
     // unsafe { println!("hit rate: {:.2}", hit as f32 / total as f32 * 100.0) };
     // println!("{:?}", state);
     // dbg!(failed.len());
@@ -112,44 +132,36 @@ fn run_recurse(state: State, program: &[u64], failed: &mut HashSet<State>) -> bo
         match instruction {
             0 => {
                 next.a >>= combo;
-                let out = run_recurse(next, program, failed);
+                let out = run_recurse(next, program, failed, failed_as);
                 
-                if out {
-                    return out;
-                }
-                
-                let lower = next.a << combo;
-                let upper = (next.a + 1) << combo;
-                
-                // dbg!(out);
-                // println!("removing: {}", upper - lower);
-                for a in lower..upper {
-                    // println!("failed: {a}");
-                    failed.insert(State {
-                        a,
-                        ..state
-                    });
-                }
+                // if out {
+                //     return out;
+                // }
+                // 
+                // let lower = next.a << combo;
+                // let upper = (next.a + 1) << combo;
+                // 
+                // failed_as.entry(state.into()).or_default().push(lower..upper);
                 
                 out
             },
             1 => {
                 next.b ^= literal;
-                run_recurse(next, program, failed)
+                run_recurse(next, program, failed, failed_as)
             },
             2 => {
                 next.b = combo % 8;
-                run_recurse(next, program, failed)
+                run_recurse(next, program, failed, failed_as)
             },
             3 => {
                 if state.a != 0 {
                     next.pointer = literal as usize;
                 }
-                run_recurse(next, program, failed)
+                run_recurse(next, program, failed, failed_as)
             },
             4 => {
                 next.b ^= state.c;
-                run_recurse(next, program, failed)
+                run_recurse(next, program, failed, failed_as)
             },
             5 => {
                 let output = combo % 8;
@@ -160,28 +172,51 @@ fn run_recurse(state: State, program: &[u64], failed: &mut HashSet<State>) -> bo
                     if next.out_index == program.len() {
                         true
                     } else {
-                        run_recurse(next, program, failed)
+                        run_recurse(next, program, failed, failed_as)
                     }
                 }
             },
             6 => {
                 next.b = state.a >> combo;
-                run_recurse(next, program, failed)
+                let out = run_recurse(next, program, failed, failed_as);
+
+                // if out {
+                //     return out;
+                // }
+                // 
+                // let lower = next.b << combo;
+                // let upper = (next.b + 1) << combo;
+                // 
+                // failed_as.entry(state.into()).or_default().push(lower..upper);
+
+                out
             }
             7 => {
                 next.c = state.a >> combo;
-                run_recurse(next, program, failed)
+                let out = run_recurse(next, program, failed, failed_as);
+
+                // if out {
+                //     return out;
+                // }
+                // 
+                // let lower = next.c << combo;
+                // let upper = (next.c + 1) << combo;
+                // 
+                // failed_as.entry(state.into()).or_default().push(lower..upper);
+
+                out
             }
             _ => panic!("{instruction}")
         }
     };
     
-    if out {
-        true
-    } else {
-        failed.insert(state);
-        false
-    }
+    out
+    // if out {
+    //     true
+    // } else {
+    //     failed.insert(state);
+    //     false
+    // }
 }
 
 fn run(mut a: u64, mut b: u64, mut c: u64, program: &[u64]) -> Vec<u64> {
@@ -236,3 +271,33 @@ fn run(mut a: u64, mut b: u64, mut c: u64, program: &[u64]) -> Vec<u64> {
 
     output
 }
+
+/*
+Program:
+0,3 // a = a >> 3
+5,4 // print(a % 8)
+3,0 // jump(0) if a != 0
+ */
+
+/*
+Program: 
+2,4, // b = a % 8
+1,7, // b = b ^ 7 = b ^ 0b111 = 7 - b
+7,5, // c = a >> b = 
+4,1, // b = b ^ c
+1,4, // b = b ^ 4
+5,5, // print(b % 8)
+0,3, // a = a >> 3
+3,0, // jump(0) if a != 0
+
+b = 8k + 2 = ...010
+b = 8k
+c = ...000
+...000
+
+
+// b = 8 * k
+// b = 8 * k + 4
+
+ */
+
