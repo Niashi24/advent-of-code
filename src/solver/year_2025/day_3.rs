f4e862eb8d8458fe4003ac9c0d952c543b33faab
Niashi <niasparacino4@gmail.com>
2025-12-03 12:25:06 -0500

    Simplify Day 3 solution

--- src/solver/year_2025/day_3.rs
+++ src/solver/year_2025/day_3.rs
@@ -1,40 +1,29 @@
 use std::io::BufRead;
+use utils::extensions::FirstMax;
 
-use itertools::Itertools;
-
-pub fn part_1(input: Box<dyn BufRead>) -> anyhow::Result<u32> {
-    let mut total = 0;
+pub fn solve(input: Box<dyn BufRead>) -> anyhow::Result<(u64, u64)> {
+    let mut p_1 = 0;
+    let mut p_2 = 0;
     for line in input.lines().map(Result::<String, _>::unwrap) {
         let digits: Vec<u32> = line.chars().map(|c| c.to_digit(10).unwrap()).collect();
 
-        let max_left = *digits[0..digits.len()-1].iter().max().unwrap();
-        let i = digits.iter().find_position(|&&x| x == max_left).unwrap().0;
-        let max_right = *digits[i+1..].iter().max().unwrap();
-        total += max_left * 10 + max_right;
-        continue;
+        p_1 += solve_n(&digits, 2);
+        p_2 += solve_n(&digits, 12);
     }
 
-    Ok(total)
+    Ok((p_1, p_2))
 }
 
-pub fn part_2(input: Box<dyn BufRead>) -> anyhow::Result<u64> {
-    let mut total = 0;
-    for line in input.lines().map(Result::<String, _>::unwrap) {
-        let digits: Vec<u32> = line.chars().map(|c| c.to_digit(10).unwrap()).collect();
-
-        let mut joltage = 0;
-        let n = 12;
-        let mut cur_range = digits.as_slice();
-        for i in 0..n {
-            let search_range = cur_range.len() - (n - i) + 1;
-            let max = cur_range[0..search_range].iter().copied().max().unwrap();
-            let j = cur_range.iter().copied().find_position(|x| *x == max).unwrap().0;
-            joltage = joltage * 10 + max as u64;
-            cur_range = &cur_range[j+1..];
-        }
-        
-        total += joltage;
+fn solve_n(digits: &[u32], n: usize) -> u64 {
+    let mut joltage = 0;
+    let mut cur_range = digits;
+    for i in 0..n {
+        let search_range = cur_range.len() - (n - i) + 1;
+        let (j, max) = cur_range[0..search_range].iter().copied().first_max().unwrap();
+        joltage = joltage * 10 + max as u64;
+        cur_range = &cur_range[j+1..];
     }
-
-    Ok(total)
+    joltage
 }
+
+// fn first_max()
--- utils/src/extensions.rs
+++ utils/src/extensions.rs
@@ -23,3 +23,24 @@ fn is_none_or_works() {
     let x: Option<u32> = None;
     assert_eq!(x.is_none_or(|x| x > 1), true);
 }
+
+pub trait FirstMax<T> {
+    fn first_max(self) -> Option<(usize, T)>;
+}
+
+impl <T: Ord, It: Iterator<Item=T>> FirstMax<T> for It {
+    fn first_max(mut self) -> Option<(usize, T)> {
+        let mut max = self.next()?;
+        let mut max_i = 0;
+
+        let mut i = 1;
+        for item in self {
+            if item > max {
+                (max, max_i) = (item, i);
+            }
+            i += 1;
+        }
+
+        Some((max_i, max))
+    }
+} 
