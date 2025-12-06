c631783d96ba33087c55b718fd3cbaf462fc1864
Niashi <niasparacino4@gmail.com>
2025-12-06 11:22:04 -0500

    Day 5

---
+++ src/solver/year_2025/day_5.rs
@@ -0,0 +1,43 @@
+use std::io::BufRead;
+
+use core::ops::RangeInclusive;
+
+use itertools::Itertools;
+
+
+pub fn part_1(input: Box<dyn BufRead>) -> anyhow::Result<(usize, usize)> {
+    let mut ranges: Vec<Ri> = Vec::new();
+    let mut lines = input.lines().map(Result::unwrap);
+    while let Some(line) = lines.next() {
+        let line: String = line;
+        if line.is_empty() { break; }
+
+        let (start, end) = line.split_once("-").unwrap();
+        let mut range = start.parse::<i64>()?..=end.parse::<i64>()?;
+        
+        while let Some((i, _)) = ranges.iter().find_position(|r| intersects(r, &range)) {
+            let r = ranges.swap_remove(i);
+            range = merge(range, r);
+        }
+
+        ranges.push(range);
+    }
+
+    let p_1 = lines.map(|s: String| s.parse::<i64>().unwrap())
+        .filter(|i| ranges.iter().any(|r| r.contains(i)))
+        .count();
+
+    let p_2 = ranges.iter().map(|r| r.end() - r.start() + 1).sum::<i64>() as usize;
+
+    Ok((p_1, p_2))
+}
+
+type Ri = RangeInclusive<i64>;
+
+fn intersects(a: &Ri, b: &Ri) -> bool {
+    a.contains(b.start()) || b.contains(a.start())
+}
+
+fn merge(a: Ri, b: Ri) -> Ri {
+    (*a.start()).min(*b.start())..=((*a.end()).max(*b.end()))
+}
--- src/solver/year_2025/mod.rs
+++ src/solver/year_2025/mod.rs
@@ -4,11 +4,13 @@ mod day_1;
 mod day_2;
 mod day_3;
 mod day_4;
+mod day_5;
 
 pub fn add_all(database: &mut SolverDatabase) {
     database.add_solver(Day::new(1, 2025), Solver::separated((day_1::part_1, day_1::part_2)));
     database.add_solver(Day::new(2, 2025), Solver::combined(day_2::solve));
     database.add_solver(Day::new(3, 2025), Solver::combined(day_3::solve));
     database.add_solver(Day::new(4, 2025), Solver::combined(day_4::solve));
+    database.add_solver(Day::new(5, 2025), Solver::combined(day_5::part_1));
 
 }
